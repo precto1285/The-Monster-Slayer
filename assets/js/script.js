edb3ca06278254215a0c055ef31c19b568928296
Philippe Recto <precto1285@gmal.com>
2018-10-02 14:10:28 -0700

    implemented Special Attack function

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,17 +13,25 @@ new Vue({
     },
     attack: function () {
       this.monsterHealth -= this.calculateDamage(3, 10);
-
       if (this.checkWin()) {
         return;
       }
 
+      this.monsterAttacks();
+    },
+    specialAttack: function () {
+      this.monsterHealth -= this.calculateDamage(10, 20);
+      if (this.checkWin()) {
+        return;
+      }
+      this.monsterAttacks();
+    },
+    heal: function () { },
+    giveUp: function () { },
+    monsterAttacks: function () {
       this.playerHealth -= this.calculateDamage(5, 12);
       this.checkWin();
     },
-    specialAttack: function () { },
-    heal: function () { },
-    giveUp: function () { },
     calculateDamage: function (min, max) {
       return Math.max(Math.floor(Math.random() * max) + 1, min);
     },
